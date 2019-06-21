e45b86426835bd5513a7a37f41a3f7c644f5d956
Cristian Martinez <2cristianmartinez@gmail.com>
2019-06-21 12:39:24 -0400

    user can set new playlist name before creating

--- client/components/SongListView.js
+++ client/components/SongListView.js
@@ -4,14 +4,52 @@ export default class SongListView extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      playlistName: ''
+      playlistName: '',
+      toggle: false
     }
+    this.toggle = this.toggle.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
+
+  toggle() {
+    this.setState({toggle: !this.state.toggle})
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  // createPlaylist(userId, this.state)
+  handleSubmit(event) {
+    event.preventDefault()
+    const {userId, createPlaylist} = this.props
+    createPlaylist(userId, this.state.playlistName)
+  }
+
   render() {
-    const {tracks, createPlaylist, userId} = props
+    const {tracks} = this.props
     const trackIds = tracks.map(track => track.id)
+
     return (
       <div>
+        <button onClick={this.toggle} type="submit">
+          toggle
+        </button>
+        {this.state.toggle && (
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="playlistName"
+              placeholder="playlist name..."
+              value={this.state.playlistName}
+              onChange={this.handleChange}
+            />
+            <button type="submit">Add to my Spotify playlists</button>
+          </form>
+        )}
         <table style={{border: '1px white solid'}}>
           <thead>
             <tr>
@@ -19,11 +57,7 @@ export default class SongListView extends React.Component {
               <th>Track</th>
               <th>Album</th>
               <th>Release Year</th>
-              <th>
-                <button type="submit" onClick={() => createPlaylist(userId)}>
-                  Add to my Spotify playlists
-                </button>
-              </th>
+              <th />
             </tr>
           </thead>
           <tbody>
