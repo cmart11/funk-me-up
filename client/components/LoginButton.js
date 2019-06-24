ad3d332658f3f249ab1d896db53f81d7d2910602
Cristian Martinez <2cristianmartinez@gmail.com>
2019-06-23 22:28:44 -0400

    changed README and added login page app description

--- client/components/LoginButton.js
+++ client/components/LoginButton.js
@@ -3,6 +3,10 @@ import React from 'react'
 export default function LoginButton() {
   return (
     <div className="login-div">
+      <h4>
+        Sign in with your Spotify account to start creating playlists based on
+        the music you're listening to now!
+      </h4>
       <button
         className="np-bttn login-div logbttn"
         type="submit"
